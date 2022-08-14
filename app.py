72ee73ca9449f2a04bc239415f4a6cf7524eee46
S.Lee <110766362+ShawnieeLee@users.noreply.github.com>
2022-08-14 15:27:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,21 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-from skimage import io
 from keras.models import load_model
-import cv2 #remove for cloud
 from PIL import Image #use PIL
 import numpy as np
+
 app = Flask(__name__)
+
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
+def init():
     if request.method == 'POST':
         file = request.files['file']
         print("File Received")
         filename = secure_filename(file.filename)
         print(filename)
-        file.save("./static/"+filename) #remove when cloud
-        file = open("./static/"+filename,"r") #remove when cloud
         model = load_model("pneumonia")
-        image = cv2.imread("./static/"+filename) #remove for cloud
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #remove for cloud
-        img = cv2.merge([gray,gray,gray]) #remove for cloud
+        image = Image.open(file)
+        img = np.asarray(image)
 # for cloud add this (cloud need to use PIL instead of CV2): image = Image.open(file)
 # for cloud add this: img = np.asarray(image)
         img.resize((150,150,3))
